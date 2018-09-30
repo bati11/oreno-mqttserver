f1b248e521e104ff015a1e3cd5b904e5b962425f
bati11 <mail.bati11@gmail.com>
2018-09-30 14:21:46 +0900

    add validation of clientId

--- mqtt/packet/connect_payload.go
+++ mqtt/packet/connect_payload.go
@@ -2,15 +2,40 @@ package packet
 
 import (
 	"encoding/binary"
+	"errors"
+	"regexp"
 )
 
 type ConnectPayload struct {
 	ClientID string
 }
 
-func ToConnectPayload(bs []byte) ConnectPayload {
-	length := binary.BigEndian.Uint16(bs[0:2])
-	return ConnectPayload{
-		ClientID: string(bs[2 : 2+length]),
+var clientIDRegex = regexp.MustCompile("^[a-zA-Z0-9]*$")
+
+var (
+	ErrConnectPayloadLength = errors.New("payload length of connect packet is invalid")
+	ErrClientIDFormat       = errors.New("clientId format shoud be \"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\"")
+)
+
+func ToConnectPayload(bs []byte) (ConnectPayload, error) {
+	if len(bs) < 3 {
+		return ConnectPayload{}, ErrConnectPayloadLength
 	}
+	length := binary.BigEndian.Uint16(bs[0:2])
+	var s string
+	if len(bs) < 2+int(length) {
+		s = string(bs[2:])
+	} else {
+		s = string(bs[2 : 2+length])
+	}
+	if len(s) < 1 || len(s) > 23 {
+		return ConnectPayload{}, ErrClientIDFormat
+	}
+	if !clientIDRegex.MatchString(s) {
+		return ConnectPayload{}, ErrClientIDFormat
+	}
+	result := ConnectPayload{
+		ClientID: s,
+	}
+	return result, nil
 }
--- mqtt/packet/connect_payload_test.go
+++ mqtt/packet/connect_payload_test.go
@@ -1,15 +1,50 @@
 package packet_test
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/bati11/oreno-mqtt/mqtt/packet"
 )
 
 func TestToConnectPayload(t *testing.T) {
-	in := []byte{0x00, 0x04, 'h', 'o', 'g', 'e'}
-	connectPayload := packet.ToConnectPayload(in)
-	if connectPayload.ClientID != "hoge" {
-		t.Errorf("connectPayload.ClientID: got %v, want %v", connectPayload.ClientID, "hoge")
+	cases := []struct {
+		in        []byte
+		want      string
+		wantError bool
+		err       error
+	}{
+		{
+			[]byte{0x00, 0x04, 'h', 'o', 'g', 'e'}, "hoge",
+			false, nil,
+		},
+		{
+			[]byte{0x00, 0x04, 'h', 'o', 'g'}, "hog",
+			false, nil,
+		},
+		{
+			[]byte{}, "",
+			true, packet.ErrConnectPayloadLength,
+		},
+		{
+			[]byte{0x00, 0x01, '*'}, "",
+			true, packet.ErrClientIDFormat,
+		},
+	}
+	for _, tt := range cases {
+		t.Run(fmt.Sprintf("%q", tt.in), func(t *testing.T) {
+			connectPayload, err := packet.ToConnectPayload(tt.in)
+			if !tt.wantError && (err != nil) {
+				t.Fatalf("want no err, but %#v", err)
+			}
+			if tt.wantError && !(reflect.DeepEqual(err, tt.err)) {
+				t.Fatalf("want %v, but %#v", tt.err, err)
+			}
+
+			if !tt.wantError && connectPayload.ClientID != tt.want {
+				t.Fatalf("connectPayload.ClientID: got %v, want %v", connectPayload.ClientID, tt.want)
+			}
+		})
 	}
 }
