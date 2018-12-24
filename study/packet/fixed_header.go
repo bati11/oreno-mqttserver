3749d2a08f7cf44289bd00e3bd3cd89e6e32b974
bati11 <mail.bati11@gmail.com>
2018-12-24 15:16:24 +0900

    study2

--- study/packet/fixed_header.go
+++ study/packet/fixed_header.go
@@ -2,10 +2,28 @@ package packet
 
 type FixedHeader struct {
 	PacketType byte
+	Dup        byte
+	QoS1       byte
+	QoS2       byte
+	Retain     byte
 }
 
 func ToFixedHeader(bs []byte) FixedHeader {
 	b := bs[0]
 	packetType := b >> 4
-	return FixedHeader{packetType}
+	dup := refbit(bs[0], 3)
+	qos1 := refbit(bs[0], 2)
+	qos2 := refbit(bs[0], 1)
+	retain := refbit(bs[0], 0)
+	return FixedHeader{
+		PacketType: packetType,
+		Dup:        dup,
+		QoS1:       qos1,
+		QoS2:       qos2,
+		Retain:     retain,
+	}
+}
+
+func refbit(b byte, n uint) byte {
+	return (b >> n) & 1
 }
--- study/packet/fixed_header_test.go
+++ study/packet/fixed_header_test.go
@@ -16,19 +16,19 @@ func TestToFixedHeader(t *testing.T) {
 		want packet.FixedHeader
 	}{
 		{
-			"Reserved",
-			args{[]byte{0x00, 0x00}},
-			packet.FixedHeader{0},
+			"Reserved Dup:0 QoS:00 Retain:0",
+			args{[]byte{0x00, 0x00}}, // 0000 0 00 0
+			packet.FixedHeader{PacketType: 0, Dup: 0, QoS1: 0, QoS2: 0, Retain: 0},
 		},
 		{
-			"CONNECT",
-			args{[]byte{0x10, 0x00}},
-			packet.FixedHeader{1},
+			"CONNECT Dup:1 QoS:01 Retain:1",
+			args{[]byte{0x1B, 0x00}}, // 0001 1 01 1
+			packet.FixedHeader{PacketType: 1, Dup: 1, QoS1: 0, QoS2: 1, Retain: 1},
 		},
 		{
-			"CONNACK",
-			args{[]byte{0x20, 0x00}},
-			packet.FixedHeader{2},
+			"CONNACK Dup:0 QoS:10 Retain:1",
+			args{[]byte{0x24, 0x00}}, // 0002 0 10 0
+			packet.FixedHeader{PacketType: 2, Dup: 0, QoS1: 1, QoS2: 0, Retain: 0},
 		},
 	}
 	for _, tt := range tests {
