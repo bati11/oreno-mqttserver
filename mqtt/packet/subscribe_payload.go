b3b95da364546f00abd976be0d159dc140303bc2
bati11 <mail.bati11@gmail.com>
2019-02-11 16:09:59 +0900

    feat: add SUBSCRIBE packet

---
+++ mqtt/packet/subscribe_payload.go
@@ -0,0 +1,80 @@
+package packet
+
+import (
+	"bufio"
+	"encoding/binary"
+	"fmt"
+	"io"
+)
+
+type TopicFilterPair struct {
+	Filter string
+	QoS    uint8
+}
+type SubscribePayload struct {
+	TopicFilterPairs []*TopicFilterPair
+}
+
+func ToSubscribePayload(fixedHeader DefaultFixedHeader, variableHeader SubscribeVariableHeader, r *bufio.Reader) (SubscribePayload, error) {
+	if fixedHeader.PacketType != SUBSCRIBE {
+		return SubscribePayload{}, fmt.Errorf("packet type is invalid. it got is %v", fixedHeader.PacketType)
+	}
+
+	var topicFilterPairs []*TopicFilterPair
+
+	remain := fixedHeader.RemainingLength - variableHeader.Length()
+	for remain > 0 {
+		length, err := extractLength(r)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return SubscribePayload{}, err
+		}
+		remain = remain - 2 - uint(length)
+
+		if remain < 0 {
+			break
+		}
+
+		bs := make([]byte, length)
+		_, err = io.ReadFull(r, bs)
+		if err != nil {
+			return SubscribePayload{}, err
+		}
+		topicFilter := string(bs)
+
+		qos, err := extractQoS(r)
+		if err != nil {
+			return SubscribePayload{}, err
+		}
+		remain--
+
+		topicFilterPairs = append(topicFilterPairs, &TopicFilterPair{topicFilter, qos})
+	}
+	return SubscribePayload{topicFilterPairs}, nil
+}
+
+func extractLength(r *bufio.Reader) (uint16, error) {
+	lengthMSB, err := r.ReadByte()
+	if err != nil {
+		return 0, err
+	}
+	lengthLSB, err := r.ReadByte()
+	if err != nil {
+		return 0, err
+	}
+	length := binary.BigEndian.Uint16([]byte{lengthMSB, lengthLSB})
+	return length, nil
+}
+
+func extractQoS(r *bufio.Reader) (uint8, error) {
+	b, err := r.ReadByte()
+	if err != nil {
+		return 0, err
+	}
+	qos1 := refbit(b, 1)
+	qos2 := refbit(b, 0)
+	qos := qos2 << qos1
+	return qos, nil
+}
---
+++ mqtt/packet/subscribe_variable_header_test.go
@@ -0,0 +1,34 @@
+package packet
+
+import (
+	"bufio"
+	"reflect"
+	"testing"
+)
+
+func TestToSubscribeVariableHeader(t *testing.T) {
+	type args struct {
+		fixedHeader DefaultFixedHeader
+		r           *bufio.Reader
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    SubscribeVariableHeader
+		wantErr bool
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ToSubscribeVariableHeader(tt.args.fixedHeader, tt.args.r)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ToSubscribeVariableHeader() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ToSubscribeVariableHeader() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
