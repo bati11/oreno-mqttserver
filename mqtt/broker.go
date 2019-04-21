27715a19f90e34e3a374acd53840cfa86e61f715
bati11 <mail.bati11@gmail.com>
2019-04-21 19:41:21 +0900

    feat: add Subscriber

--- mqtt/broker.go
+++ mqtt/broker.go
@@ -1,12 +1,20 @@
 package mqtt
 
 type Publisher <-chan []byte
+type Subscriber chan<- []byte
 
-func Broker(publishers <-chan Publisher, toSub chan<- []byte) {
-	defer close(toSub)
-	for fromPub := range publishers {
-		for message := range fromPub {
-			toSub <- message
+func Broker(publishers <-chan Publisher, subscribers <-chan Subscriber) {
+	var ss []Subscriber
+	for {
+		select {
+		case sub := <-subscribers:
+			ss = append(ss, sub)
+		case pub := <-publishers:
+			for message := range pub {
+				for _, sub := range ss {
+					sub <- message
+				}
+			}
 		}
 	}
 }
--- mqtt/broker_test.go
+++ mqtt/broker_test.go
@@ -1,15 +1,21 @@
 package mqtt
 
 import (
+	"sync"
 	"testing"
 )
 
 func TestBroker(t *testing.T) {
 	publishers := make(chan Publisher)
-	sub := make(chan []byte)
+	subscribers := make(chan Subscriber)
 
 	// "broker" goroutine
-	go Broker(publishers, sub)
+	go Broker(publishers, subscribers)
+
+	sub1 := make(chan []byte)
+	subscribers <- sub1
+	sub2 := make(chan []byte)
+	subscribers <- sub2
 
 	// "pub" goroutine
 	go func() {
@@ -26,22 +32,51 @@ func TestBroker(t *testing.T) {
 		close(pub2)
 	}()
 
+	wg := sync.WaitGroup{}
+
 	// "sub"
-	message1, ok := <-sub
-	if !ok {
-		t.Fatalf("failed read from channel")
-	}
-	if string(message1) != "hoge" {
-		t.Fatalf("got %v, want \"hoge\"", string(message1))
-	}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
 
-	message2, ok := <-sub
-	if !ok {
-		t.Fatalf("failed read from channel")
-	}
-	if string(message2) != "fuga" {
-		t.Fatalf("got %v, want \"fuga\"", string(message2))
-	}
+		message1_1, ok := <-sub1
+		if !ok {
+			t.Fatalf("failed read from channel")
+		}
+		if string(message1_1) != "hoge" {
+			t.Fatalf("got %v, want \"hoge\"", string(message1_1))
+		}
 
+		message1_2, ok := <-sub1
+		if !ok {
+			t.Fatalf("failed read from channel")
+		}
+		if string(message1_2) != "fuga" {
+			t.Fatalf("got %v, want \"fuga\"", string(message1_2))
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		message2_1, ok := <-sub2
+		if !ok {
+			t.Fatalf("failed read from channel")
+		}
+		if string(message2_1) != "hoge" {
+			t.Fatalf("got %v, want \"hoge\"", string(message2_1))
+		}
+
+		message2_2, ok := <-sub2
+		if !ok {
+			t.Fatalf("failed read from channel")
+		}
+		if string(message2_2) != "fuga" {
+			t.Fatalf("got %v, want \"fuga\"", string(message2_2))
+		}
+	}()
+
+	wg.Wait()
 	close(publishers)
 }
